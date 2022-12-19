81caca3995530bff1d3b428452c9b188eac8619c
rakeshworklab <118895794+rakeshworklab@users.noreply.github.com>
2022-12-19 19:24:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.95.0"
+      version = "~> 3.30.0"
     }
   }
 }
@@ -11,7 +11,6 @@ provider "azurerm" {
   client_secret              = "tfI8Q~s_Et_Iy2JKze2wT6v3YeyeeRNiRB4B3cCd"
   subscription_id            = "664b6097-19f2-42a3-be95-a4a6b4069f6b"
   tenant_id                  = "0adb040b-ca22-4ca6-9447-ab7b049a22ff"
-  #version                    = "~> 2.95.0"
   features {}
   skip_provider_registration = true
 }
@@ -19,36 +18,20 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
         resource_group_name  = "sa1_test_eic_trainingRakesh"
-        storage_account_name = "rakeshbackendstore"
-        container_name       = "rakeshtfstate"
+        storage_account_name = "sa1sa"
+        container_name       = "sa1sacontainer"
         key                  = "rakeshterraform.tfstate"
     }
     }
 
-resource "azurerm_app_service_plan" "rakeshservice_plan" {
-  name                = "rakeshservice_plan"
-  location            = "Southeast Asia"
-  resource_group_name = "sa1_test_eic_trainingRakesh"
+module "appservices" {
+    source = "./modules/appservices"
+    spname = var.spname 
+    location = var.location
+    #sp_id = var.sp_id
+    wanames = var.wanames
 
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
 }
-
-resource "azurerm_app_service" "rakesh_sample_webapp" {
-  name                = "rakeshsample-webapp"
-  location            = "Southeast Asia"
-  resource_group_name = "sa1_test_eic_trainingRakesh"
-  app_service_plan_id = azurerm_app_service_plan.rakeshservice_plan.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
+module "storageaccount" {
+    source = "./modules/storageaccount"
 }
-#thankou
